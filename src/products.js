63830ed382ec95d531c0ecf9c13360efe6fae989
= <peterndirangu0005@gmail.com>
2023-08-29 23:07:49 +0300

    update

--- src/products.js
+++ src/products.js
@@ -3,14 +3,24 @@ import product2 from "./assets/products/2.png";
 import product3 from "./assets/products/3.png";
 import product4 from "./assets/products/4.png";
 import product5 from "./assets/products/5.png";
+<<<<<<< Updated upstream
 import product6 from "./assets/products/6.webp";
 import product7 from "./assets/products/7.webp";
 import product8 from "./assets/products/8.webp";
+=======
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+>>>>>>> Stashed changes
 
 export const PRODUCTS = [
   {
     id: 1,
+<<<<<<< Updated upstream
     productName: "IPhone",
+=======
+    productName: "Lenovo Yoga",
+>>>>>>> Stashed changes
     price: 999.0,
     productImage: product1,
   },
