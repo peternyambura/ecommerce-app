da416b0466906207278958e6ee8f2ad215a7a47e
= <peterndirangu0005@gmail.com>
2023-08-30 09:05:04 +0300

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="links">
-        <Link to="/"> Shop </Link>
+        <Link to="/"> Home </Link>
         <Link to="/contact"> Contact </Link>
         <Link to="/cart">
           <ShoppingCart size={32} />
