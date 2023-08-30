e4099d9a2ec64cead751f6df0c0fe0c5fc4f25db
danwycliff <danwycliff6769@gmail.com>
2023-08-30 21:13:20 +0300

    codes

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -8,7 +8,7 @@ export const CartItem = (props) => {
 
   return (
     <div className="cartItem">
-      <img src={productImage} />
+      <img src={productImage} alt="img" />
       <div className="description">
         <p>
           <b>{productName}</b>
